9e3ccf5ad6bfaee3f945156f807234842874664b
Yuri Hong 홍유리 <yurihong18@gmail.com>
2025-03-15 14:53:09 +0900

    Update 2839.py

--- glasshong/01_greedy/2839.py
+++ glasshong/01_greedy/2839.py
@@ -1,1 +1,14 @@
+# 2839
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+
+if n == 1 or n == 2 or n == 4 or n == 7:
+    print(-1)
+elif n % 5 == 0:
+    print(n // 5)
+elif n % 5 == 1 or n % 5 == 3:
+    print(n // 5 + 1)
+else:
+    print(n // 5 + 2)
